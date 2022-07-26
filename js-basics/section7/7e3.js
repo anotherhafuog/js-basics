f7ea8a63008582c4ee01172150b6d8aa9a031ee1
anotherhafuog <108097380+anotherhafuog@users.noreply.github.com>
2022-07-25 21:27:57 -0700

    Add files via upload

---
+++ js-basics/section7/7e3.js
@@ -0,0 +1,19 @@
+try {
+    const numbers = [1,2,3,4];
+    const count = countOccurences(null, 1);
+    console.log(count);
+}
+
+catch (e) {
+    console.log(e.message);
+}
+
+function countOccurences(array, searchElement) {
+    if (!Array.isArray(array))
+        throw new Error('Invalid array.')
+    
+    return array.reduce((accumulator, current) => {
+        const occurence = (current === searchElement) ? 1 : 0;
+        return accumulator + occurrence;
+    }, 0);
+}
